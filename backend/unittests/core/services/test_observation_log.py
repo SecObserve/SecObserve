ecd930191a8692a2c73759a6d1400183d2720872
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2026-03-18 15:17:59 +0000

    fix: remove fell out of notifications message for obersation titles

--- backend/unittests/core/services/test_observation_log.py
+++ backend/unittests/core/services/test_observation_log.py
@@ -9,23 +9,20 @@ from unittests.base_test_case import BaseTestCase
 class TestObservationLog(BaseTestCase):
     @patch("application.core.models.Observation.save")
     @patch("application.core.models.Observation_Log.save")
-    @patch("application.core.models.Observation_Log.objects.filter")
     @patch("application.core.services.observation_log.get_current_user")
     @patch("application.core.models.Product.save")
     @patch("application.core.services.observation_log.send_observation_notification")
     @patch("application.core.services.observation_log.send_observation_title_notification")
-    def test_create_observation_log_1(
+    def test_create_observation_log(
         self,
         mock_send_observation_title_notification,
         mock_send_observation_notification,
         mock_product_save,
         mock_user,
-        mock_observation_log_filter,
         mock_observation_log_save,
         mock_observation_save,
     ):
         mock_user.return_value = self.user_internal
-        mock_observation_log_filter.return_value.count.return_value = 1
 
         observation_log = create_observation_log(
             observation=self.observation_1,
@@ -56,55 +53,4 @@ class TestObservationLog(BaseTestCase):
         mock_user.assert_called_once()
         self.observation_1.product.save.assert_called_once()
         mock_send_observation_notification.assert_called_once()
-        mock_send_observation_title_notification.assert_called_with(self.observation_1, True)
-
-    @patch("application.core.models.Observation.save")
-    @patch("application.core.models.Observation_Log.save")
-    @patch("application.core.models.Observation_Log.objects.filter")
-    @patch("application.core.services.observation_log.get_current_user")
-    @patch("application.core.models.Product.save")
-    @patch("application.core.services.observation_log.send_observation_notification")
-    @patch("application.core.services.observation_log.send_observation_title_notification")
-    def test_create_observation_log_2(
-        self,
-        mock_send_observation_title_notification,
-        mock_send_observation_notification,
-        mock_product_save,
-        mock_user,
-        mock_observation_log_filter,
-        mock_observation_log_save,
-        mock_observation_save,
-    ):
-        mock_user.return_value = self.user_internal
-        mock_observation_log_filter.return_value.count.return_value = 2
-
-        observation_log = create_observation_log(
-            observation=self.observation_1,
-            severity="severity",
-            status="status",
-            comment="comment",
-            vex_justification="vex_justification",
-            assessment_status=Assessment_Status.ASSESSMENT_STATUS_AUTO_APPROVED,
-            risk_acceptance_expiry_date=date(2024, 7, 1),
-        )
-
-        self.assertEqual(self.observation_1, observation_log.observation)
-        self.assertEqual(self.user_internal, observation_log.user)
-        self.assertEqual("severity", observation_log.severity)
-        self.assertEqual("status", observation_log.status)
-        self.assertEqual("comment", observation_log.comment)
-        self.assertEqual("vex_justification", observation_log.vex_justification)
-        self.assertEqual(
-            Assessment_Status.ASSESSMENT_STATUS_AUTO_APPROVED,
-            observation_log.assessment_status,
-        )
-        self.assertEqual(date(2024, 7, 1), observation_log.risk_acceptance_expiry_date)
-
-        self.assertEqual(self.observation_1.last_observation_log, observation_log.created)
-
-        observation_log.save.assert_called_once()
-        self.observation_1.save.assert_called_once()
-        mock_user.assert_called_once()
-        self.observation_1.product.save.assert_called_once()
-        mock_send_observation_notification.assert_called_once()
-        mock_send_observation_title_notification.assert_called_with(self.observation_1, False)
+        mock_send_observation_title_notification.assert_called_with(self.observation_1)
