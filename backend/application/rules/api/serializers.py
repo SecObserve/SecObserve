3799a4f5577f8536415981d79811719b2e2d0305
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2026-06-13 08:51:37 +0100

    chore: validate regular expressions for rules before saving

--- backend/application/rules/api/serializers.py
+++ backend/application/rules/api/serializers.py
@@ -1,4 +1,5 @@
 import platform
+import re
 from typing import Any, Optional
 
 from rest_framework.serializers import (
@@ -21,7 +22,7 @@ from application.rules.models import Rule
 from application.rules.types import Rule_Status, Rule_Type
 
 
-class GeneralRuleSerializer(ModelSerializer):
+class RuleSerializerBase(ModelSerializer):
     user = CharField(read_only=True)
     approval_status = CharField(read_only=True)
     approval_remark = CharField(read_only=True)
@@ -29,10 +30,6 @@ class GeneralRuleSerializer(ModelSerializer):
     approval_user = CharField(read_only=True)
     user_full_name = SerializerMethodField()
     approval_user_full_name = SerializerMethodField()
-
-    class Meta:
-        model = Rule
-        exclude = ["product"]
 
     def get_user_full_name(self, obj: Rule) -> Optional[str]:
         if obj.user:
@@ -67,42 +64,70 @@ class GeneralRuleSerializer(ModelSerializer):
 
         return super().validate(attrs)
 
+    def validate_title(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_description_observation(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_component_name_version(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_component_purl(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_docker_image_name_tag(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_endpoint_url(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_service_name(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_source_file(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_cloud_qualified_resource(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
+    def validate_origin_kubernetes_qualified_resource(self, value: str) -> str:
+        _validate_regex(value)
+        return value
+
     def update(self, instance: Rule, validated_data: dict) -> Rule:
         instance.approval_status = ""
         return super().update(instance, validated_data)
 
 
-class ProductRuleSerializer(ModelSerializer):
+def _validate_regex(value: str) -> None:
+    try:
+        re.compile(value, re.IGNORECASE)
+    except re.error as e:
+        raise ValidationError(f"Cannot compile regular expression: '{str(e)}'") from e
+
+
+class GeneralRuleSerializer(RuleSerializerBase):
+    class Meta:
+        model = Rule
+        exclude = ["product"]
+
+
+class ProductRuleSerializer(RuleSerializerBase):
     product_data = NestedProductSerializer(source="product", read_only=True)
-    user = CharField(read_only=True)
-    approval_status = CharField(read_only=True)
-    approval_remark = CharField(read_only=True)
-    approval_date = DateTimeField(read_only=True)
-    approval_user = CharField(read_only=True)
-    user_full_name = SerializerMethodField()
-    approval_user_full_name = SerializerMethodField()
 
     class Meta:
         model = Rule
         fields = "__all__"
-
-    def get_user_full_name(self, obj: Rule) -> Optional[str]:
-        if obj.user:
-            return obj.user.full_name
-
-        return None
-
-    def get_approval_user_full_name(self, obj: Rule) -> Optional[str]:
-        if obj.approval_user:
-            return obj.approval_user.full_name
-
-        return None
-
-    def validate_description(self, value: str) -> str:
-        if not value:
-            raise ValidationError("Must be set")
-
-        return value
 
     def validate_product(self, value: Product) -> Product:
         self.instance: Rule
@@ -110,16 +135,6 @@ class ProductRuleSerializer(ModelSerializer):
             raise ValidationError("Product cannot be changed")
 
         return value
-
-    def validate(self, attrs: dict) -> dict:
-        if attrs.get("type") == Rule_Type.RULE_TYPE_REGO and not attrs.get("rego_module"):
-            raise ValidationError("Rego module must be set")
-
-        return super().validate(attrs)
-
-    def update(self, instance: Rule, validated_data: dict) -> Rule:
-        instance.approval_status = ""
-        return super().update(instance, validated_data)
 
 
 class RuleApprovalSerializer(Serializer):
