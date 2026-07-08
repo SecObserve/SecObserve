9b2cb798ad3497efe67eef2ecdbb5987baef991c
nh4ttruong <truongtbn243@gmail.com>
2026-07-08 17:14:56 +0700

    Revert "fix: support dev startup on ARM macOS"
    This reverts commit d372fa65655cf506f5d31c172317a93de9f72879.

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-set -e
-
-case "$(uname -m)" in
-  arm64|aarch64)
-    export DOCKER_DEFAULT_PLATFORM="${DOCKER_DEFAULT_PLATFORM:-linux/amd64}"
-    ;;
-esac
-
+cd ./frontend
+npm install --no-audit --no-fund && 
+cd ..
 docker compose -f docker-compose-dev.yml up --build
