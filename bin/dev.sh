d372fa65655cf506f5d31c172317a93de9f72879
nh4ttruong <truongtbn243@gmail.com>
2026-07-08 15:31:59 +0700

    fix: support dev startup on ARM macOS

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cd ./frontend
-npm install --no-audit --no-fund && 
-cd ..
+set -e
+
+case "$(uname -m)" in
+  arm64|aarch64)
+    export DOCKER_DEFAULT_PLATFORM="${DOCKER_DEFAULT_PLATFORM:-linux/amd64}"
+    ;;
+esac
+
 docker compose -f docker-compose-dev.yml up --build
