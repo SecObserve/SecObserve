7d9a295936ab6855dfbecb8134e31381c06ff657
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2026-07-18 07:04:49 +0100

    feat: show statistics about background tasks (#4522)
    * feat: show statistics about background tasks
    
    * feat: start for ui
    
    * feat: ui step 2
    
    * feat: more functionality and unittests
    
    * feat: numbers
    
    * fix: unittests

--- backend/application/background_tasks/api/views.py
+++ backend/application/background_tasks/api/views.py
@@ -1,11 +1,21 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from huey.contrib.djhuey import HUEY as huey
+from huey.contrib.stats import enable_stats
+from peewee import SqliteDatabase
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from application.background_tasks.api.filters import PeriodicTaskFilter
-from application.background_tasks.api.serializers import PeriodicTaskSerializer
+from application.background_tasks.api.serializers import (
+    BackgroundTaskStatisticsSerializer,
+    PeriodicTaskSerializer,
+)
 from application.background_tasks.models import Periodic_Task
 from application.commons.api.permissions import UserHasSuperuserPermission
 
@@ -17,3 +27,22 @@ class PeriodicTaskViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     queryset = Periodic_Task.objects.all()
     filter_backends = [SearchFilter, DjangoFilterBackend]
     search_fields = ["task"]
+
+
+class BackgroundTaskView(APIView):
+    permission_classes = [IsAuthenticated, UserHasSuperuserPermission]
+    serializer_class = BackgroundTaskStatisticsSerializer
+
+    @action(detail=False, methods=["get"], url_name="background_task_statistics")
+    def get(self, request: Request) -> Response:
+        stats_db = SqliteDatabase("/var/lib/huey/huey_stats.db")
+        stats = enable_stats(huey, stats_db)
+
+        content = {
+            "registered": stats.task_breakdown(),
+            "throughput": stats.throughput(minutes=60),
+            "counts": stats.window_counts(seconds=86400),
+            "running": stats.inflight(),
+        }
+        serializer = BackgroundTaskStatisticsSerializer(content)
+        return Response(serializer.data)
