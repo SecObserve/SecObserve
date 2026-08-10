3daa85c2848bc6b81c6e7a904c23f3890145ccc8
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2026-08-10 19:37:53 +0100

    chore: tests and several changes

--- backend/application/rules/services/simulator.py
+++ backend/application/rules/services/simulator.py
@@ -1,19 +1,15 @@
-from copy import copy
-from typing import Tuple
-
 from django.core.paginator import Paginator
 from django.db.models import QuerySet
 
 from application.core.models import Observation
 from application.core.queries.product import get_products
-from application.core.services.observation import normalize_observation_fields
 from application.rules.models import Rule
 from application.rules.services.rule_engine import Rule_Engine
 
 MAX_OBSERVATIONS = 100
 
 
-def simulate_rule(rule: Rule) -> Tuple[int, list[Observation]]:
+def simulate_rule(rule: Rule) -> list[Observation]:
     simulation_results: list[Observation] = []
 
     observations = _get_observations(rule)
@@ -29,24 +25,7 @@ def simulate_rule(rule: Rule) -> Tuple[int, list[Observation]]:
                 rule_engine = Rule_Engine(observation.product)
                 rule_engines[observation.product.pk] = rule_engine
 
-            observation_before = copy(observation)
-
-            observation_before.rule_status = ""
-            observation_before.rule_rego_status = ""
-            observation_before.rule_severity = ""
-            observation_before.rule_rego_status = ""
-            observation_before.rule_priority = None
-            observation_before.rule_rego_priority = None
-            observation_before.rule_vex_justification = ""
-            observation_before.rule_rego_vex_justification = ""
-            observation_before.general_rule = None
-            observation_before.general_rule_rego = None
-            observation_before.product_rule = None
-            observation_before.product_rule_rego = None
-
-            normalize_observation_fields(observation_before)
-
-            if rule_engine.check_rule_for_observation(rule, observation, observation_before, True):
+            if rule_engine.check_rule_for_observation(rule, observation, None, True):
                 if len(simulation_results) < MAX_OBSERVATIONS:
                     simulation_results.append(observation)
                 if len(simulation_results) == MAX_OBSERVATIONS:
@@ -55,7 +34,7 @@ def simulate_rule(rule: Rule) -> Tuple[int, list[Observation]]:
             continue
         break
 
-    return observations.count(), simulation_results
+    return simulation_results
 
 
 def _get_observations(rule: Rule) -> QuerySet:
