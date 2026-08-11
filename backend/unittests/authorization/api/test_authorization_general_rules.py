99d092957846a9de8e5100c6733300221e99ce95
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2026-08-11 19:00:33 +0100

    chore: optimise rule simulation (#4614)
    * chore: optimise rule simulation
    
    * chore: codequality
    
    * chore: tests and several changes

--- backend/unittests/authorization/api/test_authorization_general_rules.py
+++ backend/unittests/authorization/api/test_authorization_general_rules.py
@@ -84,7 +84,7 @@ class TestAuthorizationGeneralRules(TestAuthorizationBase):
             )
         )
 
-        expected_data = "{'count': 0, 'results': []}"
+        expected_data = "{'results': []}"
         self._test_api(APITest("db_admin", "post", "/api/general_rules/3/simulate/", None, 200, expected_data))
 
         expected_data = "{'message': 'You do not have permission to perform this action.'}"
